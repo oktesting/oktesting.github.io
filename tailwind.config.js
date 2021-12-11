9ee9d435925ecd60b0f5275dbaf84939f5db7742
DucCM <duczoz123@gmail.com>
2021-12-12 01:28:56 +0700

    support dark mode and rebuild

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,15 +2,21 @@ const defaultTheme = require('tailwindcss/defaultTheme');
 module.exports = {
   mode: 'jit', // just in time mode will purge all the massive unused css sent to browser while developing
   purge: ['./**/*.html'],
-  darkMode: false, // or 'media' or 'class'
+  darkMode: 'class', // or 'media' or 'class'
   theme: {
-    backgroundColor: (theme) => ({
+    /* backgroundColor: (theme) => ({
       ...theme('colors'),
       primary: '#d9e7f8'
-    }),
+    }), */
     extend: {
+      backgroundColor: {
+        primary: '#d9e7f8',
+        'text-primary': '#4a90e2',
+        'dark-mode': '#191A19'
+      },
       colors: {
         primary: '#4a90e2',
+        'bg-primary': '#d9e7f8',
         'custom-gray': '#81899c',
         'custom-purple': '#5b6a9a'
       },
