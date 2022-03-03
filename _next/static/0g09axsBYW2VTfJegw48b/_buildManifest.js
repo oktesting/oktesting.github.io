84d3c731f8b290a35f00af334b3a41c3863f57cd
oktesting <46472479+oktesting@users.noreply.github.com>
2022-03-03 08:28:26 +0000

    Deploying to dev-preview from @ oktesting/oktesting.github.io@b869f6e909426baa423f04cbfd616cddb62ee341 🚀

--- _next/static/0g09axsBYW2VTfJegw48b/_buildManifest.js
+++
@@ -1,1 +0,0 @@
-self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/css/2068e3a6d83c38a2.css","static/chunks/pages/index-9630fa699de121d2.js"],"/404":["static/chunks/pages/404-f649e9872977da6c.js"],"/_error":["static/chunks/pages/_error-2280fa386d040b66.js"],sortedPages:["/","/404","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
